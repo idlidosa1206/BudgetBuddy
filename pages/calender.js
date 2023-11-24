42c9c574a7ee43ed2631f8289642e525c79e1440
Uttara Kamat <128808814+idlidosa1206@users.noreply.github.com>
2023-11-25 03:20:16 +0530

    Update calender.js

--- pages/calender.js
+++ pages/calender.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./calendar.css";
+import { useNavigate} from 'react-router-dom';
 
-const Calendar = () => {
-  function handleClick() {
-    alert("You clicked me!");
-  }
-
+const Calendar = (props) => {
+  const navigate= useNavigate();
+  const handleClick=async(index) => {
+    console.log(`Clicked on day ${index}`);
+    try {
+      const response = await fetch('http://localhost:3000/calendar',{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ index }),
+      });
+      if (response.ok) {
+        const responseData = await response.json(); // Parse response body as JSON
+        console.log('Server Response:', responseData);
+        alert(`Today's total expenses: ${responseData.amount}`);             
+      } else {
+        // Handle login failure
+         const errorData = await response.json(); // Parse error response body as JSON
+    console.error('Login failed:', errorData.message);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+  
   return (
     <>
       <center className="header">
@@ -17,7 +39,7 @@ const Calendar = () => {
           {Array.from({ length: 31 }, (_, index) => (
             <div className="day" key={index + 1}>
               <p>{index + 1}st</p>
-              <button className="button" onClick={handleClick}>
+              <button className="button" onClick={()=>handleClick(index+1)}>
                 View Expenses
               </button>
               <br />
@@ -26,7 +48,7 @@ const Calendar = () => {
           ))}
           <div className="day">
             <p>Total</p>
-            <button className="button" onClick={handleClick}>
+            <button className="button" >
               View Expenses
             </button>
             <br />
