d3c7e71c7e98c2c7da1725f9ab882315b936c4cd
Uttara Kamat <128808814+idlidosa1206@users.noreply.github.com>
2023-12-27 18:08:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,18 @@
 // db.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'budgetbuddy',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "budgetbuddy2.0",
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("Error connecting to MySQL:", err);
   } else {
-    console.log('Connected to MySQL database');
+    console.log("Connected to MySQL database");
   }
 });
 
