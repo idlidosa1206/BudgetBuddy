3288192771575a2bb212ec544282b3963355685a
Uttara Kamat <128808814+idlidosa1206@users.noreply.github.com>
2023-12-27 18:07:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const db = require('./db'); // Assuming you have a separate file for database connection
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const db = require("./db"); // Assuming you have a separate file for database connection
+const routes = require("./routes");
 
 const app = express();
 app.use(cors());
@@ -12,13 +12,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
-
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/',routes);
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
